dd4f3c04be4dba178bd41d4ce8b23eb46049bcb8
SpencerBouck <37308463+SpencerBouck@users.noreply.github.com>
2018-03-16 14:23:20 -0700

    Update project1.java

--- project1.java
+++ project1.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
 
-public class project1 extends JApplet
- {
-  private final int WIDTH = 800;
-  private final int HEIGHT = 340;
-
-  public void init()
-   {
-       TextPanel panel = new TextPanel();
-       getContentPane().add(panel);
-       setSize(WIDTH,HEIGHT);
-   }
- }
+public class project1
+{
+  public static void main(String[] args)
+  {
+	  JFrame frame = new JFrame("CSE 360 Project 1");
+	  TextPanel panel = new TextPanel();
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setBounds(0, 0, 800, 340);
+      frame.getContentPane().add(panel);
+      frame.pack();
+      frame.setVisible(true);
+  }
+}
