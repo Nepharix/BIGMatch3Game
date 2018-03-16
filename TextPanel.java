735d81e92244552fcba3108ebed51ebf59c8141d
SpencerBouck <37308463+SpencerBouck@users.noreply.github.com>
2018-03-16 00:14:36 -0700

    Update TextPanel.java

--- TextPanel.java
+++ TextPanel.java
@@ -1,111 +1,113 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.event.*;
-
 
 public class TextPanel extends JPanel{
-	 private JButton selectInput, swapJustification, createOutput, showAnalysis ;
+	 private JButton selectInput, swapJustification, createOutput, showAnalysis;
 	 private JPanel buttons1, mainPanel, textPanel, input;
-	 private int width, height;
-	 private controlPanel1 cPanel;
-	 private JTextField fileIn;
 	 private JTextArea textArea;
+	 private JTextField fileIn;
 	 private JLabel label1;
+	 private boolean leftJustified, hasOutput;
+	 private String fileName;
 	 
 	 public TextPanel()
 	 {
+		mainPanel = new JPanel();
 		 
-		 this.width = width;
-		 this.height = height;
-		 mainPanel = new JPanel();
+		selectInput = new JButton("Select New Input");
+		swapJustification = new JButton("Swap Justification");
+		createOutput = new JButton("Create New Output");
+		showAnalysis = new JButton("Show Analysis");
 		 
-		 selectInput = new JButton("Select New Input");
-		 //selectInput.addActionListener(new ButtonListener());
-         swapJustification = new JButton("Swap Justification");
-         //swapJustification.addActionListener(new ButtonListener());
-         createOutput = new JButton("Create New Output");
-         //createOutput.addActionListener(new ButtonListener());
-		 showAnalysis = new JButton("Show Analysis");
-		 //showAnalysis.addActionListener(new ButtonListener());
+		//listeners for the buttons
+		selectInput.addActionListener(new ButtonListener());
+		swapJustification.addActionListener(new ButtonListener());
+		createOutput.addActionListener(new ButtonListener());
+		showAnalysis.addActionListener(new ButtonListener());
 		 
-		 /*
-		 //listeners for the buttons
-		 selectInput.addActionListener(new ButtonListener());
-		 swapJustification.addActionListener(new ButtonListener());
-		 createOutput.addActionListener(new ButtonListener());
-		 showAnalysis.addActionListener(new ButtonListener());
-        */
-		 
-		 buttons1 = new JPanel();
-         buttons1.setLayout(new GridLayout(4,1));
+		buttons1 = new JPanel();
+		buttons1.setLayout(new GridLayout(4,1));
          
-         buttons1.add(selectInput).setPreferredSize(new Dimension(200,60));
-         buttons1.add(swapJustification).setPreferredSize(new Dimension(200,60));
-         buttons1.add(createOutput).setPreferredSize(new Dimension(200,60));
-         buttons1.add(showAnalysis).setPreferredSize(new Dimension(200,60));
+		buttons1.add(selectInput).setPreferredSize(new Dimension(200,60));
+		buttons1.add(swapJustification).setPreferredSize(new Dimension(200,60));
+		buttons1.add(createOutput).setPreferredSize(new Dimension(200,60));
+		buttons1.add(showAnalysis).setPreferredSize(new Dimension(200,60));
          
          
-         textPanel = new JPanel();
+		textPanel = new JPanel();
          
-         JTextArea textArea = new JTextArea();
-         textArea.setLineWrap(true);
-         textArea.setWrapStyleWord(true);
-         
-         
-         JScrollPane areaScrollPane = new JScrollPane(textArea);
-         areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-         areaScrollPane.setPreferredSize(new Dimension(250,250));
-         add(areaScrollPane);
-         
-         input = new JPanel();
-         input.setLayout(new GridLayout(2,1));
-         label1 = new JLabel("Please enter the name of your file");
-         fileIn = new JTextField();
-         input.add(label1);
-         input.add(fileIn);
-         add(input);
-         
-         mainPanel.add(input, BorderLayout.EAST);
-         mainPanel.add(buttons1, BorderLayout.WEST);
-         mainPanel.add(textPanel, BorderLayout.EAST);
-         add(mainPanel);
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
          
          
+		JScrollPane areaScrollPane = new JScrollPane(textArea);
+		areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		areaScrollPane.setPreferredSize(new Dimension(250,250));
+		add(areaScrollPane);
+         
+		input = new JPanel();
+		input.setLayout(new GridLayout(2,1));
+		label1 = new JLabel("Please enter the name of your file");
+		fileIn = new JTextField();
+		input.add(label1);
+		input.add(fileIn);
+		add(input);
+         
+		mainPanel.add(input, BorderLayout.EAST);
+		mainPanel.add(buttons1, BorderLayout.WEST);
+		mainPanel.add(textPanel, BorderLayout.EAST);
+		add(mainPanel);
+         
+		leftJustified = true;
+		hasOutput = false;
+		fileName = ""; //to flag that no file has been given
 	 }
 	
 	 private class ButtonListener implements ActionListener	{
 		public void actionPerformed (ActionEvent event)	{
-			if(event.getSource() == selectInput)	{
-				
+			
+			//selects an input file
+			if(event.getSource() == selectInput) {
+				fileName = fileIn.getText();
+				//TODO read from input file
+				textArea.setText("Input File Selected: " + fileName); //temporary test line
 			}
-			if(event.getSource() == swapJustification)	{
-				
+			
+			//swaps the justification of the file
+			else if(event.getSource() == swapJustification)	{
+				leftJustified = !leftJustified;
+				if(leftJustified) {
+					textArea.setText("Current Justification: Left");
+				}
+				else {
+					textArea.setText("Current Justification: Right");
+				}
 			}
-			if(event.getSource() == createOutput)	{
-				
+			
+			//writes to an output file
+			else if(event.getSource() == createOutput)	{
+				if(fileIn.getText().equals(fileName)) {
+					textArea.setText("Please enter a file other than the input file to output to");
+				}
+				else {
+					//TODO output to file
+					textArea.setText("Output File Selected: " + fileIn.getText()); //temporary test line
+					hasOutput = true;
+				}
 			}
-			if(event.getSource() == showAnalysis){
-				
+			
+			//shows the analysis of the file
+			else if(event.getSource() == showAnalysis){
+				if(hasOutput) {
+					//TODO display formating analysis
+					textArea.setText("Displays Analysis"); //temporary test line
+				}
+				else {
+					textArea.setText("No formating has been done yet");
+				}
 			}
 		}
 	 }
-	 
-	 }
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-
-
+}
